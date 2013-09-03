ca54bfd39427833c0afdfbefe921f5c25dc68ebd
Silas Baronda <silas.baronda@gmail.com>
2013-09-03 07:07:03 -0400

    When building build mini-s3 exe

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ LEVELDB_PREFIX=/Users/silas/opt/opt/leveldb
 export GOPATH="`pwd`:$GOPATH"
 export CGO_CFLAGS="-I$LEVELDB_PREFIX/include"
 export CGO_LDFLAGS="-L$LEVELDB_PREFIX/lib"
-go build main
+go build -o mini-s3 main
